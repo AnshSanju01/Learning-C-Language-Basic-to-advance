9159ab0ee4e2bad14721b318969e1ff2b1ce39b9
Sanjeev Rao <104066772+AnshSanju01@users.noreply.github.com>
2023-04-09 07:45:53 +0530

    Update calcGST18%.cpp

--- calcGST18%.cpp
+++ calcGST18%.cpp
@@ -5,11 +5,9 @@ void calculatePrice(float value);
 int main(){
     float value = 100.0;
     calculatePrice(value);
-
-
+    
     return 0;
 }
-
 
 void calculatePrice(float value){
     value = value + (value * 0.18);
